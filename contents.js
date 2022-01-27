75c1cbaaf8f8a127bde0eaf71062ee24614a6f7a
Megha-Gull <98393025+Megha-Gull@users.noreply.github.com>
2022-01-27 13:21:06 +0530

    Add files via upload

--- contents.js
+++ contents.js
@@ -1,33 +1,36 @@
 const contents= [
     {
-        title:'Parkour',
-        description: 'the activity or sport of moving rapidly through an area, typically in an urban environment, negotiating obstacles by running, jumping, and climbing.',
-        imagePath:'./assets/parkour.jpg',
-        source: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd'
-    },
-    {
         title:'Big Buck Bunny',
-        description: 'A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.',
+        description: 'Three rodents amuse themselves by harassing creatures of the forest. However, when they mess with a bunny, he decides to teach them a lesson.',
         imagePath:'./assets/big_bunny.jpg',
         source: 'https://www.rmp-streaming.com/media/big-buck-bunny-360p.mp4'
-    },
-    {
-        title: 'Elephants dream',
-        description:'Friends Proog and Emo journey inside the folds of a seemingly infinite Machine',
-        imagePath:'./assets/elephants_dream.jpg',
-        source:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+        
     },
     {
         title: 'Sintel',
-        description:'A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales.',
+        description:'A lonely young woman, Sintel, helps and befriends a dragon, whom she calls Scales. But when he is kidnapped by an adult dragon, Sintel decides to embark on a dangerous quest to find her lost friend Scales.',
         imagePath:'./assets/sintel.jpg',
-        source:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
+        source:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'   
+    },
+    {
+        title: 'Elephants dream',
+        description:'Friends Proog and Emo journey inside the folds of a seemingly infinite Machine, exploring the dark and twisted complex of wires, gears, and cogs, until a moment of conflict negates all their assumptions.',
+        imagePath:'./assets/elephants_dream.jpg',
+        source:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
     },
     {
         title: 'Tears of steel',
         description:'In an apocalyptic future, a group of soldiers and scientists takes refuge in Amsterdam to try to stop an army of robots that threatens the planet.',
         imagePath:'./assets/tears_of_steel.jpg',
         source: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4'
+        
+    },
+    {
+        title:'Parkour',
+        description: 'Parkour is an athletic training discipline in which practitioners move from one place to another in a complex environment, without assisting equipment and in the fastest and most efficient way possible.',
+        imagePath:'./assets/parkour.jpg',
+        source: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd'
+        
     }
 ]
 
